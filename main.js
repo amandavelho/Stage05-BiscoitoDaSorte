4c1a9d3365df06c9f27f6d3c7a5570f32fb313ec
Amanda Velho <amandalimavelho@gmail.com>
2023-10-20 10:22:41 -0300

    Parte1

---
+++ main.js
@@ -0,0 +1,6 @@
+const btnOpen = document.querySelector('#btnOpen')
+const btnReset = document.querySelector('#btnReset')
+
+const screen1 = document.querySelector('screen1')
+const screen2 = document.querySelector('screen2')
+
