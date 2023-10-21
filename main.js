649059e39a8364f5b5dffff28fe3aa7560ba79c2
Amanda Velho <amandalimavelho@gmail.com>
2023-10-21 17:27:59 -0300

    Co-authored-by: Bruno S Velho <bruno.velho@gmail.com>

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const btnOpen = document.querySelector('#btnOpen')
 const btnReset = document.querySelector('#btnReset')
 
-const screen1 = document.querySelector('screen1')
-const screen2 = document.querySelector('screen2')
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+
+//Eventos
+btnReset.addEventListener('click', tryAgain)
+btnOpen.addEventListener('click', tryAgain)
+
+//Functions
+
+function tryAgain() {
+    
+    screen2.classList.toggle('hide')
+    screen1.classList.toggle('hide')
+}
 
